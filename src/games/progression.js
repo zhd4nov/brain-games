562c4457c5b2b888a5fb3bedf71da075abe54c5d
zhd4nov <prctmlm@gmail.com>
2019-11-30 17:29:02 +0300

    add a new scenario of game

---
+++ src/games/progression.js
@@ -0,0 +1,42 @@
+import { generateRandomNumber } from './even.js';
+import { pickMathOperator } from './calc.js';
+
+export const tellProgressionRules = () => console.log(`What number is missing in the progression?`);
+
+export const prepareQuestionProgressiveString = () => {
+  const progressionArray = generateProgression();
+  const randomHiddenElementIndex = generateRandomNumber(progressionArray.length);
+
+  const prograssionArrayWithHiddenElement = progressionArray.slice();
+  const correctAnswer = prograssionArrayWithHiddenElement.splice(randomHiddenElementIndex, 1, '..');
+
+  const correctAnswerString = String(correctAnswer);
+  const readyProgressionString = prograssionArrayWithHiddenElement.join(' ');
+
+  return [readyProgressionString, correctAnswerString];
+};
+
+const calculateNextNumber = (progression, mathOperator = '+', step = 1) => {
+  switch (mathOperator) {
+    case '-':
+      return progression[progression.length - 1] - step;
+    case '+':
+      return progression[progression.length - 1] + step;
+  }
+};
+
+const generateProgression = (maxLength = 10) => {
+  const startNumber = generateRandomNumber();
+  const mathOperator = pickMathOperator(['+', '-']);
+  const progressionStep = generateRandomNumber();
+
+  const bunchUpNumbers = (progression = []) => {
+    if (progression.length === maxLength) {
+      return progression;
+    }
+    const nextNumber = calculateNextNumber(progression, mathOperator, progressionStep);
+    return bunchUpNumbers([...progression, nextNumber]);
+  };
+
+  return bunchUpNumbers([startNumber]);
+};
