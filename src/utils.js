c4e661a577762541d6f1ab33ca95167212825625
zhd4nov <prctmlm@gmail.com>
2019-12-05 00:37:12 +0300

    chore(utils.js) add a new tool

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,13 @@
-export const generateRandomNumber = (maxNumber = 300) => {
+export const generateRandomNumber = (maxNumber) => {
   const perhapsNumber = Math.floor(Math.random() * Math.floor(maxNumber));
   const readyNumber = perhapsNumber !== 0 ? perhapsNumber : generateRandomNumber();
 
   return readyNumber;
+};
+
+export const pickMathOperator = (operators) => {
+  const operatorsNumber = operators.length;
+  const randomPickIndex = Math.floor(Math.random() * Math.floor(operatorsNumber));
+
+  return operators[randomPickIndex];
 };
