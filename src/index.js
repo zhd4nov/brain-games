8bdbea1746ccdecbe9ed8962db83eeb663792f9e
zhd4nov <prctmlm@gmail.com>
2019-12-04 23:11:07 +0300

    refactor(game core) rewrite engine
    It was changed order communication modules
    It was excluded need to use logic in execution files with OCP

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import { sayWelcome, tellRules, requestUserName, greetUser, startGame } from './engine';
 
-// main flow export:
-export const start = (gameType) => {
-  sayWelcome();
-  tellRules(gameType);
+import readlineSync from 'readline-sync';
 
-  const userName = requestUserName();
-  greetUser(userName);
+export const tellRules = (rulles) => console.log(`Welcome to the Brain Games!\n${rulles}`);
 
-  startGame(gameType, userName);
+export const requestUserName = () => readlineSync.question('\nMay I have your name? ');
 
-  return;
+const checkUserAnswer = (correctAnswer, userAnswer) => correctAnswer === userAnswer;
+
+export const startRound = (userName, questionGenerator, roundLimitCounter = 0) => {
+  if (roundLimitCounter === 3) {
+    console.log(`Congratulations, ${userName}!`);
+    return;
+  }
+
+  const questionPack = questionGenerator();
+  console.log(`Question: ${questionPack.question}`);
+  const userAnswer = readlineSync.question('Your answer: ');
+
+  const result = checkUserAnswer(questionPack.answer, userAnswer);
+  if (!result) {
+    console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${questionPack.answer}".\nLet's try again, ${userName}!`);
+    return;
+  }
+  console.log('Correct!');
+
+  return startRound(userName, questionGenerator, roundLimitCounter + 1);
 };
