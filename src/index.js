dda02d42e5b516f4723e75319f4cb922bdc43c47
zhd4nov <prctmlm@gmail.com>
2019-12-05 01:52:14 +0300

    refactor(game engine) simplify export

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 
 import readlineSync from 'readline-sync';
 
-export const tellRules = (rulles) => console.log(`Welcome to the Brain Games!\n${rulles}`);
-
-export const requestUserName = () => readlineSync.question('\nMay I have your name? ');
+const tellRules = (rulles) => console.log(`Welcome to the Brain Games!\n${rulles}`);
 
 const checkUserAnswer = (correctAnswer, userAnswer) => correctAnswer === userAnswer;
 
-export const startRound = (userName, questionGenerator, roundLimitCounter = 0) => {
+const startRound = (userName, questionGenerator, roundLimitCounter = 0) => {
   if (roundLimitCounter === 3) {
     console.log(`Congratulations, ${userName}!`);
     return;
@@ -25,4 +23,15 @@ export const startRound = (userName, questionGenerator, roundLimitCounter = 0) =
   console.log('Correct!');
 
   return startRound(userName, questionGenerator, roundLimitCounter + 1);
+};
+
+export const startGame = (rulles, questionGenerator) => {
+  tellRules(rulles);
+
+  const userName = readlineSync.question('\nMay I have your name? ');
+  console.log(`Hello, ${userName}!\n`);
+
+  startRound(userName, questionGenerator);
+
+  return;
 };
