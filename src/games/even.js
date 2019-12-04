2a138a1ac01ad26b19a6401b63fac5d5383ab3a5
zhd4nov <prctmlm@gmail.com>
2019-12-04 23:22:18 +0300

    refactor(even.js) normalize for new engine

--- src/games/even.js
+++ src/games/even.js
@@ -1,8 +1,7 @@
 import { generateRandomNumber } from '../utils';
+import { tellRules, requestUserName, startRound } from '..';
 
-export const tellEvenRulles = () => console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-export const prepareQuestionNumber = () => {
+const prepareQuestionNumber = () => {
   const randomNumber = generateRandomNumber()
   const correctAnswerString = randomNumber % 2 === 0 ? 'yes' : 'no';
   const questionNumberString = String(randomNumber);
@@ -12,4 +11,16 @@ export const prepareQuestionNumber = () => {
   };
 
   return questionPack;
+};
+
+export const startEvenGame = () => {
+  const evenRulles = 'Answer "yes" if the number is even, otherwise answer "no".';
+  tellRules(evenRulles);
+
+  const userName = requestUserName();
+  console.log(`Hello, ${userName}!\n`);
+
+  startRound(userName, prepareQuestionNumber)
+
+  return;
 };
