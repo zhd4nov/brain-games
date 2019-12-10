56fc21a623f74b1d7e0336d5295ddefb6981e2bd
w-zhd4nov <prctmlm@gmail.com>
2019-12-10 18:04:15 +0300

    fix(random) change generator

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 export const generateRandomNumber = (min, max) => {
-  const randomNumber = Math.floor(min + Math.random() * Math.floor(max + 1));
+  const randomNumber = Math.floor(min + Math.random() * Math.floor(max));
   return randomNumber;
 };
 
@@ -8,15 +8,4 @@ export const pickMathOperator = (operators) => {
   const randomPickIndex = Math.floor(Math.random() * Math.floor(operatorsNumber));
 
   return operators[randomPickIndex];
-};
-
-export const prepareQuestionSingleNumber = (checker) => {
-  const questionNumber = generateRandomNumber(1, 250);
-  const correctAnswer = checker(questionNumber) ? 'yes' : 'no';
-  const questionPack = {
-    question: questionNumber,
-    answer: correctAnswer,
-  };
-
-  return questionPack;
 };
