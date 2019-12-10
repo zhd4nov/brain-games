0e0f3ac3fe77c4c3941bc53568497a95e30532fb
w-zhd4nov <prctmlm@gmail.com>
2019-12-10 18:53:16 +0300

    refactor(utils.js) remove unnecessory tool

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,6 @@
-export const generateRandomNumber = (min, max) => {
+const generateRandom = (min, max) => {
   const randomNumber = Math.floor(min + Math.random() * Math.floor(max));
   return randomNumber;
 };
 
-export const pickMathOperator = (operators) => {
-  const operatorsNumber = operators.length;
-  const randomPickIndex = Math.floor(Math.random() * Math.floor(operatorsNumber));
-
-  return operators[randomPickIndex];
-};
+export default generateRandom;
